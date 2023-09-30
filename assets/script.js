681e9f3b2eca46cf9ec73ef80e4e033601204c43
abenedetti27 <145300062+abenedetti27@users.noreply.github.com>
2023-09-30 12:10:03 -0700

    skeleton html, css and pseudocode

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+//Start the game with "Begin button
+    //add eventlistener to button
+    //first question and multiplpe choice ansers populate = query selector or variable?
+    //timer starts counting down in intervals of 1000 ms
+    //Start new game when game is over
+//comapre user selected anser to correct answer = event listener?
+    //if answer is correct, next question populates
+    //if answer is wrong, time is deducted from timer, next question populates =set interval, clear interval
+//if timer runs out, game is over -lose
+//if there is time remaining that is user score, game is over -win
+//when game is won, wins are incremented to scoreboard and saved to local storage
+//when game is lost, losses are incremented to scoreboard and save to local storage
